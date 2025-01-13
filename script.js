b4617fa260ec4f06ba8b7b2e525e887cd69f6502
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-13 16:18:42 +0100

    Add element input + button [html] to [JS]

---
+++ script.js
@@ -0,0 +1,9 @@
+// Selezione degli elementi in html
+
+const distance = document.getElementById('distanza')
+const age = document.getElementById('eta')
+const button = document.getElementById('btn')
+
+console.log(button);
+console.log(distance);
+console.log(age);
