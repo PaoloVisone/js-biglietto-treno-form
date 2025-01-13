3589b0b533a97e3cad44dced8cce9c387a7a06a1
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-13 18:54:37 +0100

    Add print final price to the user

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-// Selezione degli elementi in html
+// Selezione degli elementi in html (input)
 
-const distance = document.getElementById('distanza')
-const age = document.getElementById('eta')
-const button = document.getElementById('btn')
+
+const distance = document.getElementById('distanza');
+const age = document.getElementById('eta');
+const button = document.getElementById('btn');
+const nameSurname = document.getElementById('nome-cognome');
 
 console.log(button);
 console.log(distance);
 console.log(age);
 
+// Selezione degli elementi in html (output)
+
+const name = document.getElementById('nome');
+const offer = document.getElementById('offerta');
+const wagon = document.getElementById('carrozza');
+const price = document.getElementById('prezzo')
+
 // Calcolo del biglietto 
 
 
 // Prezzo del biglietto
-let price = 0.21;
+let ticketPrice = 0.21;
 
 // Sconto minorenni
 let minDiscount = 20;
@@ -32,7 +41,7 @@ button.addEventListener('click', () => {
     console.log(old);
 
     // Calcolo prezzo x chilometri
-    const sum = price * kilometers
+    const sum = ticketPrice * kilometers
 
     // Calcolo somma x lo sconto 'minori'
     const firstDiscount = sum * minDiscount / 100;
@@ -49,10 +58,11 @@ button.addEventListener('click', () => {
 
     // If statement per calcolare il prezzo finale 
     if (old < 18) {
-        console.log(finalFirstDiscount);
+        price.innerHTML = finalFirstDiscount.toFixed(2) + " " + "€";
     } else if (old > 65) {
-        console.log(finalSecondDiscount);
+        price.innerHTML = finalSecondDiscount.toFixed(2) + " " + "€";
     } else {
-        console.log(sum);
+        price.innerHTML = sum.toFixed(2) + " " + "€";
     }
-})
+});
+
